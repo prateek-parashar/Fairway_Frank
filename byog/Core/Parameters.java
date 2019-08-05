c50d53c4370216b8ba2975b22700af9c9813ca5c
prateek-parashar <parashar4111@gmail.com>
2019-08-05 21:14:02 +0530

    Create a singleton class with shared variables. Add a method to draw the room.

--- byog/Core/Parameters.java
+++ byog/Core/Parameters.java
@@ -4,12 +4,48 @@ import byog.TileEngine.TERenderer;
 import byog.TileEngine.TETile;
 import byog.TileEngine.Tileset;
 
+import java.util.Random;
+
 public class Parameters {
-    private Room baseWorld;
+    private static final Parameters baseParameters = new Parameters();
+    private Room baseWorld = new Room(40, 80, new Point(0, 0));
     private static long SEED;
+    Random randomGenerator = new Random(SEED);
     TERenderer tileRenderer = new TERenderer();
     TETile[][] world = new TETile[baseWorld.getWidth()][baseWorld.getHeight()];
     TETile floor = Tileset.FLOOR;
     TETile wall = Tileset.WALL;
     TETile player = Tileset.PLAYER;
+
+    public static Parameters getBaseParameters() {
+        return baseParameters;
+    }
+
+    public Room getBaseWorld() {
+        return baseWorld;
+    }
+
+    public static long getSEED() {
+        return SEED;
+    }
+
+    public TERenderer getTileRenderer() {
+        return tileRenderer;
+    }
+
+    public TETile[][] getWorld() {
+        return world;
+    }
+
+    public TETile getFloor() {
+        return floor;
+    }
+
+    public TETile getWall() {
+        return wall;
+    }
+
+    public TETile getPlayer() {
+        return player;
+    }
 }
--- byog/Core/Room.java
+++ byog/Core/Room.java
@@ -1,5 +1,11 @@
 package byog.Core;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import static byog.Core.Parameters.getBaseParameters;
+
 public class Room {
     private int height;
     private int width;
@@ -48,13 +54,69 @@ public class Room {
         this.centre = centre;
     }
 
+    /** Returns the ratio of the height and width of the room */
     public double roomRatio() {
         return this.getHeight() / this.getWidth();
     }
 
-    public void drawRoom() {
+    /** Splits the given room vertically with a random chosen point in the x axis
+     * @return - list of the 2 new Room objects
+     */
+    public List<Room> verticalSplit() {
+        ArrayList<Room> returnList = new ArrayList<>();
+        Room baseRoom = this;
+        int breakPoint = getBaseParameters().randomGenerator.nextInt(baseRoom.getWidth());
 
+        Point leftCorner1 = baseRoom.getLeftCorner();
+        Point leftCorner2 = new Point(baseRoom.getLeftCorner().getX() + breakPoint, baseRoom.getLeftCorner().getY());
+
+        int width1 = leftCorner2.getX() - leftCorner1.getX();
+        int width2 = baseRoom.getWidth() - breakPoint;
+
+        Room room1 = new Room(baseRoom.getHeight(), width1, leftCorner1);
+        Room room2 = new Room(baseRoom.getHeight(), width2, leftCorner2);
+
+        if((room1.roomRatio() > 0.45) &&  (room2.roomRatio() > 0.45)) {
+            returnList.add(room1);
+            returnList.add(room2);
+        }
+        return this.verticalSplit();
     }
+
+    /** Splits the given room horizontally with a random chosen point in the y axis
+     * @return - list of the 2 new Room objects
+     */
+    public List<Room> horizontalSplit() {
+        ArrayList<Room> returnList = new ArrayList<>();
+        Room baseRoom = this;
+        int breakPoint = getBaseParameters().randomGenerator.nextInt(baseRoom.getWidth());
+
+        Point leftCorner1 = baseRoom.getLeftCorner();
+        Point leftCorner2 = new Point(baseRoom.getLeftCorner().getX(), baseRoom.getLeftCorner().getY() + breakPoint);
+
+        int height1 = leftCorner2.getY() - leftCorner1.getY();
+        int height2 = baseRoom.getHeight() - breakPoint;
+
+        Room room1 = new Room(height1, baseRoom.getWidth(), leftCorner1);
+        Room room2 = new Room(height2, baseRoom.getWidth(), leftCorner2);
+
+        if((room1.roomRatio() > 0.45) &&  (room2.roomRatio() > 0.45)) {
+            returnList.add(room1);
+            returnList.add(room2);
+        }
+        return this.horizontalSplit();
+    }
+
+    /** Used to draw the current room with a Floor tile */
+    public void drawRoom() {
+        for (int i = this.getLeftCorner().getX(); i < this.getWidth(); i++) {
+            for (int j = this.getLeftCorner().getY(); j < this.getHeight(); j++) {
+                getBaseParameters().world[i][j] = getBaseParameters().getFloor();
+            }
+        }
+    }
+
+
 }
 
 
